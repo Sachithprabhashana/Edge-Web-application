495b767b5d784e79338a0afe922f3d86d953f777
sachith <sachithprabhashana@gmail.com>
2025-02-06 09:13:49 +0530

    initial commit
    initial

---
+++ edge-web-app/src/component/admin-panel/AuthProvider.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { SessionProvider } from 'next-auth/react'
+import React, { ReactNode } from 'react'
+
+interface PropsType {
+    children: ReactNode
+}
+
+export const AuthProvider = ({ children }: PropsType) => {
+    return (
+        <SessionProvider>{children}</SessionProvider>
+    )
+
+}
